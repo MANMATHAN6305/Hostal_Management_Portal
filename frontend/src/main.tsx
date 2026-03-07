94d86eca9c495d69e4fdba3a1179b655b2fbb74a
SMANMATHAN <manmadhansiva2005@gmail.com>
2026-03-07 12:09:11 +0530

    Add deep-link 404 fallback redirect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const REDIRECT_PARAM = '__redirect'
+
+const restoreRedirectedPath = () => {
+  if (typeof window === 'undefined') return
+
+  const url = new URL(window.location.href)
+  const redirectTarget = url.searchParams.get(REDIRECT_PARAM)
+  if (!redirectTarget) return
+
+  const targetUrl = new URL(redirectTarget, window.location.origin)
+  window.history.replaceState({}, '', `${targetUrl.pathname}${targetUrl.search}${targetUrl.hash}`)
+}
+
+restoreRedirectedPath()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
